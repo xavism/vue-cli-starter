84232079e9cd4378f9d2b35815e0fced8f9179d6
alberss <albertomendezhdz@gmail.com>
2017-03-23 09:27:08 +0100

    Added Create container + component and refactor side icon display

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/containers/Hello'
 import List from '@/containers/List'
+import Create from '@/containers/Create'
 import Edit from '@/containers/Edit'
 
 Vue.use(Router)
@@ -9,6 +10,7 @@ Vue.use(Router)
 const routes = [
   { path: '/', component: Hello, title: 'Home', tab: false },
   { path: '/chars', component: List, title: 'Chars', tab: true },
+  { path: '/create', component: Create, title: 'Create', tab: false },
   { path: '/edit/:characterId', component: Edit, title: 'Edit', tab: false, props: true }
 ]
 
